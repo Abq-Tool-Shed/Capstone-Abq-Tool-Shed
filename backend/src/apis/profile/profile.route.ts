c70bafc17225a7a16d9b9f157194ce05757ae44f
Mike <MZbrozek@cnm.edu>
2022-02-17 14:10:45 -0700

    Built sign-up controller, and sign-in controller, added isLoggedIn controller and multer for images

--- backend/src/apis/profile/profile.route.ts
+++ backend/src/apis/profile/profile.route.ts
@@ -2,7 +2,7 @@ import {getProfileByProfileId, putProfileController} from "./profile.controller"
 import {Router} from "express";
 import {asyncValidatorController} from "../../utils/controllers/asyncValidator.controller";
 import {check, checkSchema} from "express-validator";
-import {isLoggedIn} from "../..utils/controllers/isLoggedIn.controler";
+import {isLoggedIn} from "../../utils/controllers/isLoggedIn.controller";
 import {profileValidator} from "./profile.validator";
 
 export const ProfileRoute: Router = Router();
