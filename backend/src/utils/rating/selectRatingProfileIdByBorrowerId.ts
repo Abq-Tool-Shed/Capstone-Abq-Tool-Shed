1c0d6dcdd610472c445c37775b023e821ddb60d9
Austin Dewey <adewey3@cnm.edu>
2022-02-21 16:23:48 -0700

    rating docs

--- backend/src/utils/rating/selectRatingProfileIdByBorrowerId.ts
+++ backend/src/utils/rating/selectRatingProfileIdByBorrowerId.ts
@@ -3,11 +3,15 @@ import { Rating } from "../interfaces/Rating"
 import { connect } from "../database.utils"
 import { RawDataPacket } from "mysql2"
 
-export async function selectRatingProfileIdByBorrowerId {
+export async function selectRatingProfileIdByBorrowerId (borrowProfileId: string|null, borrowId: string|null)  : Promise<Rating|null>{
 
     try {
         const mySqlConnect = await connect()
         const MySqlQuery = "SELECT BIN_TO_UUID(ratingProfileId) AS ratingProfileId, BIN_TO_UUID(borrowId) AS borrowId, borrowId = UUID_TO_BIN(:borrowId)"
-        const Rating
+        const result = await mySqlConnect.execute(mySqlQuery, {borrowProfileId}) as RawDataPacket[]
+        const Profile.Length Array<Borrow> = result[0] as Array<Borrow>
+        return Profile.length === 1 ? {...Borrow[0]} : null;
+    } catch(error) {
+        throw error
     }
 }
