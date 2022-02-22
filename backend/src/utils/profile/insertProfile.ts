d13cc9ae758cd5e82857fb952ba8de8426729fed
Mike <MZbrozek@cnm.edu>
2022-02-22 15:19:22 -0700

    Sign-up works in insomnia

--- backend/src/utils/profile/insertProfile.ts
+++ backend/src/utils/profile/insertProfile.ts
@@ -4,8 +4,8 @@ import {Profile} from "../interfaces/Profile";
 export async function insertProfile(profile: Profile) : Promise<string> {
     try {
         const mySqlConnection = await connect();
-
-        const query : string = 'INSERT INTO profile(profileId, profileActivationToken, profileEmail, profileHandle, profileHash, profileImage, profileName, profilePhoneNumber ) VALUES (UUID_TO_BIN(UUID()), :profileActivationToken, :profileEmail, :profileHandle, :profileProfileHash, :profileProfileImage, :profileName, :profilePhoneNumber)';
+        console.log(profile)
+        const query : string = 'INSERT INTO profile(profileId, profileActivationToken, profileEmail, profileHandle, profileHash, profileImage, profileName, profilePhoneNumber ) VALUES (UUID_TO_BIN(UUID()), :profileActivationToken, :profileEmail, :profileHandle, :profileHash, :profileImage, :profileName, :profilePhoneNumber)';
 
         await mySqlConnection.execute(query,profile);
         return 'Profile Successfully Created'
