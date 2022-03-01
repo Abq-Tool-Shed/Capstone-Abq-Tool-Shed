33d8a54d1ddf5ac91c28b9ed3451bbedae1c06b1
Austin Dewey <adewey3@cnm.edu>
2022-03-01 16:25:26 -0700

    react update

--- frontend/src/ui/Pages/Home.js
+++ frontend/src/ui/Pages/Home.js
@@ -8,13 +8,12 @@ import {Container} from "react-bootstrap";
 export function PreHome() {
     return (
         <>
-
+            <PreNavBar></PreNavBar>
             <Container>
             <div>
                 <header  style={{padding: 50,color: 'black',backgroundImage: 'url(https://www.incimages.com/uploaded_files/image/1920x1080/getty_494605768_2000133320009280151_316966.jpg)'}}>
             <h1 className={"text-center"} >ABQ Tool Shed</h1>
                 </header>
-
             </div>
             </Container>
 
