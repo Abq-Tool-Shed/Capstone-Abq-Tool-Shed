b6beb78f51c60a5a05614f4cac46da5c01e81c13
Mike Zbrozek <mzbrozek@cnm.edu>
2022-03-02 22:32:47 -0700

    Added How it works and structure for Mission Statement sections on Homepage

--- frontend/src/ui/Pages/Home.js
+++ frontend/src/ui/Pages/Home.js
@@ -1,7 +1,9 @@
 import React from "react"
 import {NavBar} from '../Components/NavBar'
-import {Container} from "react-bootstrap";
+import {Container, Row, Col} from "react-bootstrap";
 import {HowItWorks} from "../Components/HowItWorks";
+import toolSplash from "../images/lachlan-donald-YVT5aF2QM7M-unsplash "
+
 
 
 
@@ -17,6 +19,50 @@ export function Home() {
             </div>
             </Container>
 
+
+            <div>
+                <Container>
+                    <Row>
+                        <Col>
+                            <h2>How it works</h2>
+                            <p>ABQ tool shed connects Albuquerque community members with the tools they need. </p>
+                            <p>Save money, share knowledge and build community</p>
+                            <ul>
+                                <li>Create a profile, and fill you Tool Box with your garden sheers, chainsaw or air compressor</li>
+                                <li>Connect with members offering to lend a tool in your area</li>
+                                <li>Message them privately through our secure app</li>
+                                <li>Be sure to leave a rating once the tool is returned</li>
+                            </ul>
+                        </Col>
+                    </Row>
+                </Container>
+            </div>
+
+                <Container>
+                    <h1>Placeholder for featured tool listings</h1>
+                </Container>
+
+              <div>
+                  <Container>
+                      <Row className="ms-auto">
+                          <h2>Mission Statement</h2>
+                      </Row>
+                      <Row>
+                          <Image fluid className={"mx-auto"} src={toolSplash}></Image>
+                          <Col>
+
+                          </Col>
+                      </Row>
+                  </Container>
+              </div>
+
+
+
+
         </>
     )
 }
+
+
+
+
