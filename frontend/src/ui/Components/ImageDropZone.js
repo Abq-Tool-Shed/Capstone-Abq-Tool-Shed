cac1a14394c24b7068187c97d3071dde476381cb
Austin Dewey <adewey3@cnm.edu>
2022-03-03 18:39:41 -0700

    needed to git add a few files, this is the latest commit

--- frontend/src/ui/Components/ImageDropZone.js
+++ frontend/src/ui/Components/ImageDropZone.js
@@ -1,4 +1,41 @@
+
 import React, {useCallback} from 'react'
-inport {useDropzone} from 'react-dropzone'
+import {useDropzone} from 'react-dropzone'
+
+export default function ImageUploader() {
+}
 
 
+export function ImageDropZone({formikProps}) {
+
+
+    const onDrop = useCallback(acceptedFiles => {
+
+       // const formData = new FormData();
+       // formData.append('image', acceptedFiles[0]);
+
+
+       // formikProps.setFieldValue(formikProps.fieldValue, formData)
+
+    }, []);
+    const {getRootProps, getInputProps, isDragActive} = useDropzone({onDrop});
+
+
+    return (
+        <div className="form-group" {...getRootProps()}>
+            <div style={{borderRadius: 250}} className="input-group input-group-lg  border border-5 w-25 " >
+                <input className="form-control-file"
+                    accept="image/*"
+                   // onChange={formikProps.handleChange}
+                    //onBlur={formikProps.handleBlur}
+                    {...getInputProps()}/>
+                {
+
+                    isDragActive ?
+                        <p>Place Image here</p> :
+                        <p> Drag an Image Here  <br></br> Click To Add An Image </p>
+                }
+            </div>
+        </div>
+    )
+}
--- frontend/src/ui/Pages/Home.js
+++ frontend/src/ui/Pages/Home.js
@@ -1,7 +1,7 @@
 import React from "react"
-import {NavBar} from '../Components/NavBar'
+
 import {Container} from "react-bootstrap";
-import {HowItWorks} from "../Components/HowItWorks";
+
 
 
 
@@ -12,7 +12,7 @@ export function Home() {
             <Container>
             <div>
                 <header  style={{padding: 50,color: 'black',backgroundImage: 'url(https://www.incimages.com/uploaded_files/image/1920x1080/getty_494605768_2000133320009280151_316966.jpg)'}}>
-            <h1 className={"text-center"} >ABQ Tool Shed</h1>
+            <h1 className={"text-center"}>ABQ Tool Shed</h1>
                 </header>
             </div>
             </Container>
