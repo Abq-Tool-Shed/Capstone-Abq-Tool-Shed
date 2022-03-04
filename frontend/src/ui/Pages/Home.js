b973e5169524e58e16eeeff9b216a223d28ae477
Mike Zbrozek <mzbrozek@cnm.edu>
2022-03-03 22:10:17 -0700

    Changed some of the about us layout, added some images. And am trying to figure out a gradient fill.

--- frontend/src/ui/Pages/Home.js
+++ frontend/src/ui/Pages/Home.js
@@ -1,8 +1,10 @@
 import React from "react"
 import {NavBar} from '../Components/NavBar'
-import {Container, Row, Col} from "react-bootstrap";
+import {Container, Row, Col, Image} from "react-bootstrap";
 import {HowItWorks} from "../Components/HowItWorks";
-import toolSplash from "../images/lachlan-donald-YVT5aF2QM7M-unsplash "
+import toolSplash from "../Components/Images/lachlan-donald-unsplash.jpg"
+import kitten1 from "../Components/Images/img.png"
+import workingTogether from "../Components/Images/annie-gray-unsplash.jpg"
 
 
 
@@ -22,9 +24,15 @@ export function Home() {
 
             <div>
                 <Container>
-                    <Row>
-                        <Col>
-                            <h2>How it works</h2>
+                    <Row className={"my-2"}>
+                    <h2 className={"me-auto"} >How it works</h2>
+                    </Row>
+                    <Row className={"my-2"}>
+                        <Col  md={"5"} m={"2"}>
+                            <Image fluid src={workingTogether} alt={"tools from Unsplash photograph by annie-gray"}/>
+                        </Col>
+                        <Col md={"7"}>
+
                             <p>ABQ tool shed connects Albuquerque community members with the tools they need. </p>
                             <p>Save money, share knowledge and build community</p>
                             <ul>
@@ -36,27 +44,132 @@ export function Home() {
                         </Col>
                     </Row>
                 </Container>
-            </div>
+            </div> {/* End How it Works content */}
 
                 <Container>
                     <h1>Placeholder for featured tool listings</h1>
-                </Container>
+                </Container> {/* Featured tool listings */}
 
               <div>
                   <Container>
                       <Row className="ms-auto">
                           <h2>Mission Statement</h2>
                       </Row>
-                      <Row>
-                          <Image fluid className={"mx-auto"} src={toolSplash}></Image>
-                          <Col>
+                      <Row class="view rounded z-depth-1 m-3">
+                          <Image fluid className={"mx-auto"} src={toolSplash} alt={"tools from Unsplash photograph by lachlan-donald"}/>
+                          <a>
+                              <div className="mask purple-gradient-rgba d-flex align-items-end justify-content-start text-white">
+                                  <p className="font-weight-bold h5 ml-3"> ABQ tool shed saves homeowners money by offering them an option outside of the big box stores.</p>
 
+                                  <p className="font-weight-bold h5 ml-3"> To get tools out of the garage and into people's hands. </p>
+                                  <p className="font-weight-bold h5 ml-3"> From cutting tile to cutting the lawn, people should have access to the tools they need to get the job done.</p>
+                              </div>
+                          </a>
+                         <Row>
+                          <Col>
+                              <p>ABQ tool shed saves homeowners money by offering them an option outside of the big box stores.</p>
+                              <p> To get tools out of the garage and into people's hands. </p>
+                              <p>From cutting tile to cutting the lawn, people should have access to the tools they need to get the job done. </p>
                           </Col>
+                         </Row>
                       </Row>
                   </Container>
               </div>
 
+            <div>
+                <Container>
+                    <Row>
+                        <h2>FAQ</h2>
+                    </Row>
+                    <Row>
+                        <Col>
 
+
+                        </Col>
+                    </Row>
+
+                </Container>
+
+            </div> {/* End FAQ Content */}
+
+
+            <div className={"bg-secondary"} >
+                <Container>
+                    <Row>
+                        <h2>About our team</h2>
+
+                        <Col md={"3"}>
+                            <Image className={"d-block mx-auto"} fluid roundedCircle src={kitten1} alt={"kitten"}/>
+                            <div className={"bg-secondary rounded mt-5"}>
+                                <p>What a cute kitten</p>
+                            </div>
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col md={"8"}>
+                            <div className={"border border-2 rounded border-dark bg-gradient"}>
+                            <Row>
+                                <Col>
+                                    <h4>Austin Dewey</h4>
+                                </Col>
+                                <Col>
+                                    <p>test</p>
+                                </Col>
+                                {/*linkedIn profile links will go next to your name*/}
+                            </Row>
+                                <Col>
+                                <p>Short bio statement, with an interesting quirk about you.</p>
+                                </Col>
+                            </div>
+                        </Col>
+                        <Col md={'2'}>
+                            <Image className={"my-auto"} fluid roundedCircle src={kitten1} alt={"image of Austin"}/>
+                        </Col>
+
+                        <Row>
+                            <Col md={'2'}>
+                                <Image className={"my-auto"} fluid roundedCircle src={kitten1} alt={"image of Jon"}/>
+                            </Col>
+                            <Col md={"8"}>
+                                <div className={"border border-2 rounded border-dark bg-gradient"}>
+                                <Row>
+                                    <Col>
+                                        <h4> Jon Verduzco </h4>
+                                    </Col>
+                                    <Col>
+                                        <p>test</p>
+                                    </Col>
+                                    {/*linkedIn profile links will go next to your name*/}
+                                </Row>
+                                </div>
+                                <Col>
+                                    <p>Short bio statement, with an interesting quirk about you.</p>
+                                </Col>
+
+                            </Col>
+                        </Row>
+                        <Row >
+                            <Col md={'8'}>
+                                <Row>
+                                    <Col>
+                                    <h4> Mike Zbrozek </h4>
+                                    </Col>
+                                    <Col>
+                                    <p>test</p>
+                                    </Col>
+                                    {/*linkedIn profile links will go next to your name*/}
+                                </Row>
+                                <p>Full stack developer. Albuquerque resident, inspired by community solutions and empowerment. Has concerns about lending his circular saw on this site.</p>
+                            </Col>
+                        <Col md={'2'}>
+                            <Image className={"my-auto"} fluid roundedCircle src={kitten1} alt={"image of Mike"}/>
+                        </Col>
+
+                        </Row>
+
+                    </Row>
+                </Container> {/* End about us content */}
+            </div>
 
 
         </>
