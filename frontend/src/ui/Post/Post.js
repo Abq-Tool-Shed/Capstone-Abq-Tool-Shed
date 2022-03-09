fb30b767fae39f38d57e65e4c3dfa4e5bee60d09
Mike Zbrozek <mzbrozek@cnm.edu>
2022-03-09 06:04:48 -0700

    Replace missing dockerfiles

--- frontend/src/ui/Post/Post.js
+++ frontend/src/ui/Post/Post.js
@@ -2,7 +2,7 @@ import React from "react"
 import "../styles/Post.css"
 import {Col, Container, Row, Button, Form} from "react-bootstrap";
 import "../styles/Post.css"
-import {postFAQ} from "../Components/PostFAQ";
+// import {postFAQ} from "../Components/PostFAQ";
 import toolsIMG from "../Components/Images/tech-tools1.jpg"
 import {Image} from "@material-ui/icons";
 import {ImageDropZone} from "../Components/ImageDropZone";
