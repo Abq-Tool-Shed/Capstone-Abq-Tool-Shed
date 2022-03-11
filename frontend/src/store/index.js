547f8fa2534c3e3f1e85d842b7582b7a78a7c690
Mike Zbrozek <mzbrozek@cnm.edu>
2022-03-11 10:24:23 -0700

    added borrows to combined reducers in store/index.js

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -5,12 +5,10 @@ import auth from "./auth"
 import profile from "./profiles"
 
 
-const reducer = combineReducers({tools, auth, profile})
+const reducer = combineReducers({tools, borrows, auth, profile})
 
 import borrows from "./borrows";
-import auth from "./auth";
 
-const reducer = combineReducers({tools, borrows, auth})
->>>>>>> frontEnd_mz
+
 export const store = configureStore({reducer})
 
