4a661c1d871cb42b6fe7c845c010e41c2b2dfcb9
Jonathan Verduzco <jverduzco4>
2022-03-15 17:07:08 -0600

    styling

--- frontend/src/ui/Components/Shared/ProfileJumbo.js
+++ frontend/src/ui/Components/Shared/ProfileJumbo.js
@@ -6,16 +6,17 @@ export function ProfileJumbo({display1, display2, heading2, heading3, heading4, 
 
     return (
         <div className={JumboStyles.colorJumbo}>
-            <Container>
-                <div>
-                    <img className="align-content-center me-5 ms-5" style={{aspectRatio: "1/1", maxWidth: "250"}}  src={Image} alt={ImageAlt}/>
+            <Container className={"d-flex justify-content-center align-items-center flex-md-column"}>
                     <h2 className={"text-sm-center m-2 p-3 display-1"} >{display1}</h2>
                     <h2 className={"text-sm-center m-2 p-3 display-2"} >{display2}</h2>
+                    <img className="" style={{ maxWidth: "350px"}}  src={Image} alt={ImageAlt}/>
                     <h2 className={"text-sm-start m-2 p-3 "} >{heading2}</h2>
                     <h2 className={"text-sm-center m-2 p-3 "} >{heading3}</h2>
                     <h2 className={"text-sm-end m-2 p-3 "} >{heading4}</h2>
-                </div>
             </Container>
         </div>
     )
 }
+
+// aspectRatio: "1 / 1"
+// Never set absolute heights
