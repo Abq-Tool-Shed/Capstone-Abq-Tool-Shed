cce24128ce853956a04a4a5b384b7b8b85b967cf
Jonathan Verduzco <jverduzco4>
2022-03-16 16:43:22 -0600

    merge conflicts

--- backend/src/utils/tool/selectToolsByToolProfileId.ts
+++ backend/src/utils/tool/selectToolsByToolProfileId.ts
@@ -6,7 +6,7 @@ import {RowDataPacket} from "mysql2";
 export async function selectToolsByToolProfileId(toolProfileId: string) : Promise<Tool> {
     try {
         const mySqlConnection = await connect();
-        const mySqlQuery = 'SELECT BIN_TO_UUID(toolId) AS toolId, BIN_TO_UUID(toolProfileId) AS toolProfileId, BIN_TO_UUID(toolCategoryId) as toolCategoryId, toolDescription, toolLendRules, toolName, toolPostDate, toolImage AS userTools FROM tool WHERE toolProfileId = UUID_TO_BIN(:toolProfileId)'
+        const mySqlQuery = 'SELECT BIN_TO_UUID(toolId) AS toolId, BIN_TO_UUID(toolProfileId) AS toolProfileId, BIN_TO_UUID(toolCategoryId) as toolCategoryId, toolDescription, toolLendRules, toolName, toolPostDate, toolImage FROM tool WHERE toolProfileId = UUID_TO_BIN(:toolProfileId)'
         const result = await <RowDataPacket>mySqlConnection.execute(mySqlQuery, {toolProfileId})
         await mySqlConnection.release()
         return result[0] as Tool
